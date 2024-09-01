4db3905813c5a8cf8e937a139bb9fa5f006f9408
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-09-01 20:18:52 -0300

    Concluído com a classe, e com as verificações

--- CalculaNota.cs
+++ CalculaNota.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CalculaNota
+﻿namespace CalculaNota
 {
     internal class CalculaNota
     {
+        public string Nome;
+        public double N1;
+        public double N2;
+        public double N3;
+        public double P1 = 30;
+        public double P2 = 35;
+        public double P3 = 35;
+
+        public CalculaNota(string nome, double n1, double n2, double n3)
+        {
+            Nome = nome;
+            N1 = n1;
+            N2 = n2;
+            N3 = n3;
+            }
+
+        public double Calculo()
+        {
+            return CalcularMediaPonderada();
+            }
+
+        public double CalcularMediaPonderada()
+        {
+            double somaPonderada = (N1 * P1) + (N2 * P2) + (N3 * P3);
+            double somaPesos = P1 + P2 + P3;
+            double mediaPonderada = somaPonderada / somaPesos;
+            return mediaPonderada;
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace CalculaNota
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CalculaNota cn = new CalculaNota("Adriano", 90, 81, 80);
+            
+            double notaFinal = cn.Calculo();
+            double notaMinima = 60;
+
+            if (notaFinal >= notaMinima)
+            {
+                Console.WriteLine($"Você foi aprovado Sua nota foi de: {notaFinal}!");
+            }
+            else
+            {
+                double pontosFaltantes = notaMinima - notaFinal;
+                Console.WriteLine($"Você foi reprovado! Faltaram pra sua aprovação: : {pontosFaltantes}");
+            }
         }
     }
 }
